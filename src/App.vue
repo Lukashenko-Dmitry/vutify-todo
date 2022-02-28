e85356800687388e329bbd922e36e3df95fe5f01
Dmitry Lukashenko <d.a.lukashenko@gmail.com>
2022-02-28 12:53:29 +0200

    add header style & title

--- src/App.vue
+++ src/App.vue
@@ -37,14 +37,41 @@
       </v-list>
     </v-navigation-drawer>
 
-    <v-app-bar app>
+    <v-app-bar
+      app
+      color="primary"
+      dark      
+      src="https://picsum.photos/1920/1080?random"      
+      prominent
+    >
+      <template v-slot:img="{ props }">
+        <v-img
+          v-bind="props"
+          gradient="to top right, rgba(19,84,122,.5), rgba(128,208,199,.8)"
+        ></v-img>
+      </template>
+
       <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
 
-      <v-toolbar-title>Application</v-toolbar-title>
+      <v-app-bar-title>Vuetify Todo</v-app-bar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-btn icon>
+        <v-icon>mdi-magnify</v-icon>
+      </v-btn>
+
+      <v-btn icon>
+        <v-icon>mdi-heart</v-icon>
+      </v-btn>
+
+      <v-btn icon>
+        <v-icon>mdi-dots-vertical</v-icon>
+      </v-btn>
     </v-app-bar>
 
     <v-main>
-      <router-view></router-view>>
+      <router-view></router-view>
     </v-main>
   </v-app> 
 </template>
@@ -57,7 +84,7 @@ export default {
   data: () => ({
     drawer: null,
     items: [
-          { title: 'Todo', icon: 'mdi-view-dashboard', to:'/' },
+          { title: 'Todo', icon: 'mdi-format-list-checks', to:'/' },
           { title: 'About', icon: 'mdi-help-box', to:'/about' },
         ],
     right: null,
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,7 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="about pa-6">
+    <h1>About Vuetify Todo</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo, explicabo. Hic dignissimos voluptatem sapiente alias consequuntur rem impedit mollitia, blanditiis harum! Aut enim sed qui ipsa facilis, explicabo omnis eveniet!</p>
+
   </div>
 </template>
