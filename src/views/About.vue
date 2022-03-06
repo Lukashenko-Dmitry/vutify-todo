4256bb78e85de4b799e031c865c83d7b27bb390f
Dmitry Lukashenko <d.a.lukashenko@gmail.com>
2022-03-06 10:02:30 +0200

    add about

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,7 +1,21 @@
 <template>
   <div class="about pa-6">
     <h1>About {{$store.state.appTitle}}</h1>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo, explicabo. Hic dignissimos voluptatem sapiente alias consequuntur rem impedit mollitia, blanditiis harum! Aut enim sed qui ipsa facilis, explicabo omnis eveniet!</p>
+    <p>This application was created by student of Levi9 FE Academy Dmitry Lukashenko during the 
+      "Create a Gorgeous Todo App with Vuetify, Vue JS & Vuex - using many different Vuetify Components!" Udemy course
+    </p>
+    <p>
+      During the creation of this application, the following was used:
+    </p>
+    <ul>
+      <li>Vuetify</li>
+      <li>Vue Routes</li>
+      <li>Vuex</li>
+      <li>Date fsn library</li>
+      <li>Environment variable</li>
+      <li>Vue draggable</li>
+      <li>Localbase</li>
+    </ul>
 
   </div>
 </template>
